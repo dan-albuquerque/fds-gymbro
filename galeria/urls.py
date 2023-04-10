8b9463595134ec352b7369038774c460a50264e9
gabriel <gabrielrossiter04@hotmail.com>
2023-04-10 11:48:24 -0300

    colocando o css na pagina

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from galeria.views import index
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', index),
 ]
--- setup/settings.py
+++ setup/settings.py
@@ -114,6 +114,12 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+#media
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIa_URL = "/media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
