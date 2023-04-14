14731fed7d556be3c0850c18a717b2bef16c9dac
Gabriel Pires <gabrielpires2991@gmail.com>
2023-04-14 16:23:19 -0300

    Integração de treinos e exercícios com o SQLite

--- galeria/models.py
+++ galeria/models.py
@@ -13,13 +13,15 @@ class Treinos(models.Model):
     foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
     descricao = models.TextField(null=False, blank=False)
     categoria = models.CharField(max_length=100, choices=OPCOES_TREINOS, default='')
+    grupo = models.CharField(max_length=100, null=False, blank=False, default='')
 
 class Exercise(models.Model):
     name = models.CharField(max_length=100) # exercise name
     group = models.CharField(max_length=50) # group ?= peitoral, costas ou perna
     sets = models.IntegerField() # sets number
     reps = models.IntegerField() # reps number
-    #weight = models.IntegerField()
+    rest = models.IntegerField(default=40) # rest time in secs
+    weight = models.IntegerField(default=1) # weight in kg
     
     def __str__(self):
         return self.name
