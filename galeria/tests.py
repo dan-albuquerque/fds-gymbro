46393028db199b9af63293394fe1361d55005fa7
Gabriel Pires <gabrielpires2991@gmail.com>
2023-04-26 22:27:00 -0300

    Primeiro teste com Selenium

--- galeria/tests.py
+++ galeria/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # Create your tests here.
+class TestHome(LiveServerTestCase):
+    browser = webdriver.Chrome()
+
+    def test_title(self):
+        self.browser.get('http://127.0.0.1:8000/')
+        assert "Gym Bro" in self.browser.title
+    
+    '''
+    def test_link(self):
+        self.browser.get('')
+        logo = self.browser.find_element(By.CLASS_NAME, 'logo')
+        assert logo.get_attribute('href') == "https://www.django"
+    '''
+    
