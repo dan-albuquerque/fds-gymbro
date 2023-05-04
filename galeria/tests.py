1bec5eeee9a5516aef85760a564bb5fe592aa7fd
CarlosAugustoP <capv2004@gmail.com>
2023-05-03 23:59:35 -0300

    DOIS TESTES PRONTOS NO SELENIUM

--- galeria/tests.py
+++ galeria/tests.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.by import By
 
 
 
+
 class TestHome(LiveServerTestCase):
 
 
@@ -31,19 +32,16 @@ class TestHome(LiveServerTestCase):
        password_input.send_keys("123")
        submit_button.click()
 
-
-       wait = WebDriverWait(browser, 10)
-       home_title = wait.until(EC.title_contains("Gym Bro"))
-
-
+       logout_link = browser.find_element(By.LINK_TEXT, "Logout")
+       logout_link.click()
        assert "Gym Bro" in browser.title
 
 
-       browser.get("http://127.0.0.1:8000/treinos")
+       browser.quit()
 
-
-       login_title = wait.until(EC.title_contains("Gym Bro"))
-       assert "Gym Bro" in browser.title
+   def test_execucao(self):
+       browser = webdriver.Chrome()
+       browser.get("http://127.0.0.1:8000/")
 
 
        username_input = browser.find_element(By.NAME, "username")
@@ -51,20 +49,22 @@ class TestHome(LiveServerTestCase):
        submit_button = browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
 
 
-       username_input.send_keys("danilo")
-       password_input.send_keys("123")
+       username_input.send_keys("kanyewest")
+       password_input.send_keys("runaway.")
        submit_button.click()
 
+       treino_link = browser.find_element(By.LINK_TEXT, "Treinos")
+       treino_link.click()
 
-       restricted_title = wait.until(EC.title_contains("Restricted Page"))
+       treino_selecionado_link = browser.find_element(By.XPATH, "//div[@class='card']//h5[contains(text(), 'Treino de Peitoral')]")
+       treino_selecionado_link.click()
 
+       execucao_button = browser.find_element(By.XPATH, "//button[text()='video']")
 
-       assert "Restricted Page" in browser.title
+       execucao_button.click()
+       video =browser.find_element(By.CSS_SELECTOR,"iframe[src*='https://www.youtube.com/embed/sqOw2Y6uDWQ']")
+       video.click()
 
-
-       logout_link = browser.find_element(By.LINK_TEXT, "Logout")
-       logout_link.click()
-       login_title = wait.until(EC.title_contains("Gym Bro"))
        assert "Gym Bro" in browser.title
 
 
