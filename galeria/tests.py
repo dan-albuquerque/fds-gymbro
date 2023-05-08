44037ee76f289529cada31ba78e96b82790c37ee
Gabriel Pires <gabrielpires2991@gmail.com>
2023-05-08 14:14:14 -0300

    Ajeitando erro no test_resistencia em tests.py OK

--- galeria/tests.py
+++ galeria/tests.py
@@ -10,7 +10,6 @@ from selenium.webdriver.common.by import By
 
 class TestHome(LiveServerTestCase):
 
-
     def test_title(self):
         browser = webdriver.Chrome()
         browser.get('http://127.0.0.1:8000/')
@@ -69,39 +68,39 @@ class TestHome(LiveServerTestCase):
 
 
         browser.quit()
-
-def test_resistencia(self):
-        browser = webdriver.Chrome()
-        browser.get("http://127.0.0.1:8000/")
-
-
-        username_input = browser.find_element(By.NAME, "username")
-        password_input = browser.find_element(By.NAME, "password")
-        submit_button = browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
-
-
-        username_input.send_keys("danilo")
-        password_input.send_keys("123")
-        submit_button.click()
-
-        treino_link = browser.find_element(By.LINK_TEXT, "Treinos")
-        treino_link.click()
-
-        resistencia = browser.find_element(By.ID, "resistencia")
-        resistencia.click()
-
-        botao_submit = browser.find_element(By.XPATH, "//button[@type='submit']")
-        botao_submit.click()
-
-        treino_peitoral = browser.find_element(By.ID, "Treine")
-        treino_peitoral.click()
-
-        reps = browser.find_element(By.XPATH, "//th[contains(text(), 'Reps')]/following-sibling::td")
-        self.assertEqual(reps.text, "15")
-        rest = browser.find_element(By.XPATH, "//th[contains(text(), 'Rest')]/following-sibling::td")
-        self.assertEqual(rest.text, "60s")
-        assert "Gym Bro" in browser.title
-
-
         
-        browser.quit()
+    def test_resistencia(self):
+            browser = webdriver.Chrome()
+            browser.get("http://127.0.0.1:8000/")
+
+
+            username_input = browser.find_element(By.NAME, "username")
+            password_input = browser.find_element(By.NAME, "password")
+            submit_button = browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
+
+
+            username_input.send_keys("danilo")
+            password_input.send_keys("123")
+            submit_button.click()
+
+            treino_link = browser.find_element(By.LINK_TEXT, "Treinos")
+            treino_link.click()
+
+            resistencia = browser.find_element(By.ID, "resistencia")
+            resistencia.click()
+
+            botao_submit = browser.find_element(By.XPATH, "//button[@type='submit']")
+            botao_submit.click()
+
+            treino_peitoral = browser.find_element(By.ID, "Treine")
+            treino_peitoral.click()
+
+            reps = browser.find_element(By.XPATH, "//tbody/tr[1]/td[3]")
+            self.assertEqual(reps.text, "15")
+            rest = browser.find_element(By.XPATH, "//tbody/tr[1]/td[4]")
+            self.assertEqual(rest.text, "60s")
+            assert "Gym Bro" in browser.title
+
+
+            
+            browser.quit()
