dc64103e6331d9daa9f95ca8c2145a26d80ebad4
Gabriel Pires <gabrielpires2991@gmail.com>
2023-05-17 19:54:55 -0300

    Removido argumento headless no chrome options

--- galeria/tests.py
+++ galeria/tests.py
@@ -10,7 +10,7 @@ import time
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 browser = webdriver.Chrome(options=chrome_options)
 
